abc8d782087e0036350e9e939ed34e009727fc62
chenliushan <liushan_chen@outlook.com>
2016-03-18 16:06:12 +0800

    add read classpath from Idea file (.iml)

--- src/main/java/polyu_af/utils/ReadFileUtils.java
+++ src/main/java/polyu_af/utils/ReadFileUtils.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import polyu_af.domain.FaultFile;
 import polyu_af.domain.FaultUnit;
@@ -55,8 +56,13 @@ public class ReadFileUtils {
             Gson gson = new Gson();
             InputFile inputFile = gson.fromJson(input, InputFile.class);
             logger.info("InputFile:" + inputFile.toString());
-            if(inputFile.getProjectDir()!=null){
-                inputFile=readClasspathE(inputFile,inputFile.getProjectDir());
+            if (inputFile.getProjectDir() != null) {
+                if (inputFile.getSourcepathEntries() == null) {
+                    inputFile = readClasspathI(inputFile, inputFile.getProjectDir());
+                }
+                if (inputFile.getSourcepathEntries() == null) {
+                    inputFile = readClasspathE(inputFile, inputFile.getProjectDir());
+                }
             }
             logger.info("InputFile:" + inputFile.toString());
             return inputFile;
@@ -95,12 +101,12 @@ public class ReadFileUtils {
     /*
     get classpathEntries & sourcepathEntries from eclipse's .classpath file
      */
-    public static InputFile readClasspathE(InputFile inputFile,String path) {
-        List<String> classpathEntries=new ArrayList<String>();
-        List<String> sourcepathEntries=new ArrayList<String>();
+    public static InputFile readClasspathE(InputFile inputFile, String path) {
+        List<String> classpathEntries = new ArrayList<String>();
+        List<String> sourcepathEntries = new ArrayList<String>();
         try {
-            File iFile = new File(path+"/.classpath");
-            if(iFile!=null){
+            File iFile = new File(path + "/.classpath");
+            if (iFile != null) {
                 DocumentBuilderFactory dbFactory
                         = DocumentBuilderFactory.newInstance();
                 DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -110,28 +116,91 @@ public class ReadFileUtils {
                 NodeList nList = doc.getDocumentElement().getElementsByTagName("classpathentry");
                 for (int temp = 0; temp < nList.getLength(); temp++) {
                     Element eElement = (Element) nList.item(temp);
-                    if(eElement.getAttribute("kind").equals("lib")){
+                    if (eElement.getAttribute("kind").equals("lib")) {
                         classpathEntries.add(eElement.getAttribute("path"));
                     }
-
-                    if(sourcepathEntries.isEmpty()&&eElement.getAttribute("kind").equals("src")){
-                        String spE=eElement.getAttribute("path");
-                        if(!spE.startsWith("/")){
-                           spE=path+"/"+spE;
-                        }
-                        sourcepathEntries.add(spE);
+                    if (sourcepathEntries.isEmpty() && eElement.getAttribute("kind").equals("src")) {
+                        String spE = eElement.getAttribute("path");
+                        sourcepathEntries.add(joinDir(path, spE));
                     }
                 }
             }
         } catch (Exception e) {
 
         }
-        if(!classpathEntries.isEmpty()){
+        if (!classpathEntries.isEmpty()) {
             inputFile.setClasspathEntries(classpathEntries.toArray(new String[classpathEntries.size()]));
         }
-        if(!sourcepathEntries.isEmpty()){
+        if (!sourcepathEntries.isEmpty()) {
             inputFile.setSourcepathEntries(sourcepathEntries.toArray(new String[sourcepathEntries.size()]));
         }
         return inputFile;
+    }
+
+    /*
+    get classpathEntries & sourcepathEntries from Idea's .iml file
+     */
+    public static InputFile readClasspathI(InputFile inputFile, String path) {
+        List<String> classpathEntries = new ArrayList<String>();
+        List<String> sourcepathEntries = new ArrayList<String>();
+        try {
+            String projectName = path.substring(path.lastIndexOf('/') + 1);
+
+            File iFile = new File(path + "/" + projectName + ".iml");
+            if (iFile != null) {
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(iFile);
+                doc.getDocumentElement().normalize();
+                Element rootElement = (Element) doc.getDocumentElement();
+                NodeList componentList = rootElement.getElementsByTagName("component");
+                Element component = (Element) componentList.item(0);
+                NodeList contentList = component.getElementsByTagName("content");
+                Element content = (Element) contentList.item(0);
+                NodeList sourceFolderList = content.getElementsByTagName("sourceFolder");
+                Element sourceFolder = (Element) sourceFolderList.item(0);
+
+                String spathNodeValue = sourceFolder.getAttribute("url");
+                String sourcepath = spathNodeValue.substring(spathNodeValue.indexOf("$MODULE_DIR$") + 12);
+//                logger.info("sourcepath: " + joinDir(path,sourcepath));
+                sourcepathEntries.add(joinDir(path, sourcepath));
+
+
+                NodeList libraryList = component.getElementsByTagName("library");
+                for (int temp = 0; temp < libraryList.getLength(); temp++) {
+                    Element library = (Element) libraryList.item(temp);
+                    Element classNode = (Element) library.getElementsByTagName("CLASSES").item(0);
+                    Element pathNode = (Element) classNode.getElementsByTagName("root").item(0);
+                    String pathNodeValue = pathNode.getAttribute("url");
+                    String classpath = pathNodeValue.substring(pathNodeValue.indexOf("$MODULE_DIR$") + 12);
+
+                    if (classpath.endsWith("!/")) {
+                        classpath = classpath.substring(0, classpath.length() - 2);
+                    }
+//                    logger.info("classpath: " +joinDir(path,classpath) );
+                    classpathEntries.add(joinDir(path, classpath));
+
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        if (!classpathEntries.isEmpty()) {
+            inputFile.setClasspathEntries(classpathEntries.toArray(new String[classpathEntries.size()]));
+        }
+        if (!sourcepathEntries.isEmpty()) {
+            inputFile.setSourcepathEntries(sourcepathEntries.toArray(new String[sourcepathEntries.size()]));
+        }
+        return inputFile;
+    }
+
+    public static String joinDir(String path, String subPath) {
+        if (!subPath.startsWith("/") && !path.endsWith("/")) {
+            subPath = path + "/" + subPath;
+        } else {
+            subPath = path + subPath;
+        }
+        return subPath;
     }
 }
